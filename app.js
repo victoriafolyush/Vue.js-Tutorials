3598a0cdf54c449e19e6983356f1481f1dac0482
victoriafolyush <victoriafolyush@gmail.com>
2019-10-15 22:42:05 +0300

    example 3-4

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
-   el: '#vue-app',
-   data: {
-       name: 'Shaun'
-   }
+  el: '#vue-app',
+  data: {
+    name: 'Shaun',
+    job: 'Ninja',
+    website: 'http://iot.lviv.ua/',
+    websiteTag: '<a href = "http://iot.lviv.ua/">Another secret place</a>'
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good ' + time + ', ' + this.name;
+    }
+  }
 });
