a3514a89d9e5cc763d728c0ab8c4ee4c318d6309
victoriafolyush <victoriafolyush@gmail.com>
2019-10-15 23:41:27 +0300

    example 5-6

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Shaun',
-    job: 'Ninja',
-    website: 'http://iot.lviv.ua/',
-    websiteTag: '<a href = "http://iot.lviv.ua/">Another secret place</a>'
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
-    greet: function(time) {
-      return 'Good ' + time + ', ' + this.name;
+    add: function(inc) {
+      this.age += inc;
+    },
+    subtract: function(dec) {
+      this.age -= dec;
+    },
+    updateXY:function(event){
+        this.x = event.offsetX;
+        this.y = event.offsetY
+    },
+    alertMassege: function() {
+      alert('U clicked, but it still not working');
     }
   }
 });
