0c09c36fd09c33ecbe1428e6f16688397f2f472d
victoriafolyush <victoriafolyush@gmail.com>
2019-11-05 15:40:57 +0200

    lesson 10

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 new Vue({
   el: '#vue-app',
   data: {
-
+    available: true,
+    nearby: false
   },
   methods: {
-    logName: function() {
-      console.log("you entered the name")
-    },
-    logAge: function() {
-      console.log("you entered the age")
-    }
+
   }
 });
