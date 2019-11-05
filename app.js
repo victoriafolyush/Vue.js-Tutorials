c63629febcb59b2f84cb31453aaaf6db0eef1694
victoriafolyush <victoriafolyush@gmail.com>
2019-11-05 17:36:16 +0200

    lesson-9 (Computed Properties)

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 new Vue({
-  el: '#vue-app',
-  data: {
-    available: true,
-    nearby: false
-  },
-  methods: {
-
-  }
+    el: '#vue-app',
+    data: {
+        a: 0,
+        b: 0,
+        age: 20
+    },
+    methods: {
+        /*addToA: function(){
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function(){
+            console.log('addToB');
+            return this.b + this.age;
+        }*/
+    },
+    computed: {
+        addToA: function(){
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function(){
+            console.log('addToB');
+            return this.b + this.age;
+        }
+    }
 });
